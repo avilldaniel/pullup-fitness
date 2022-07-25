d40f1ef6de43fe3c84ebafb3312cce80454c99bd
Daniel Villegas <avill.daniel@gmail.com>
2022-07-24 20:40:07 -0700

    feat: am able to query db and post/register new user

--- src/pages/api/new-user.ts
+++ src/pages/api/new-user.ts
@@ -1,24 +1,62 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { useRouter } from "next/router";
+import * as z from "zod";
 import { prisma } from "../../utils/db";
 
-// const newUser = await prisma.user.create({
-//   data: {
-//     email: data.email,
-//     name: data.name,
-//     username: data.username,
-//   },
-// });
-// const users = await prisma.user.findMany();
-// console.log(users);
-const handle = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { body } = req;
-  return res.status(200).json(body);
-  // const user = await prisma.user.create({
-  //   data: {
-  //     email:
-  //   }
-  // })
-};
+// model User {
+//   id              Int               @id @default(autoincrement())
+//   memberSince     DateTime          @default(now())
+//   username        String            @db.VarChar(50) @unique
+//   email           String            @db.VarChar(50) @unique
+//   name            String?           @db.VarChar(50)
+//   exerciseStats   Exercise_stats[]
+//   workouts        Workout[]
+//   @@index([username])
+// }
+export const schema = z.object({
+  email: z.string().email().trim().max(50, "Email cannot exceed 50 characters"),
+  name: z
+    .string()
+    .min(1, "Name must be at least 1 character")
+    .max(50, "Name cannot exceed 50 characters")
+    .optional(),
+  username: z
+    .string()
+    .min(5, "Username must be at least 5 characters")
+    .max(50, "Username cannot exceed 50 characters"),
+});
+type Schema = z.infer<typeof schema>;
 
-export default handle;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // test if data in request is valid and formatted
+  try {
+    const user = schema.parse(req.body);
+    res.send(user);
+
+    // if data passes validation, invoke db query with it
+    postUser(user);
+  } catch (error) {
+    // error handling is taken from Zod's docs
+    if (error instanceof z.ZodError) {
+      console.log(error.issues);
+      res.status(400).send(error.issues);
+    }
+  }
+  // res.send(data);
+  return;
+  // res.status(200).send(data);
+}
+
+const postUser = async (user: Schema) => {
+  console.log(user);
+  const newUser = await prisma.user.create({
+    data: {
+      email: user.email,
+      name: user.name,
+      username: user.username,
+    },
+  });
+  console.log("newUser:", newUser);
+};
--- src/pages/u/register.tsx
+++ src/pages/u/register.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { prisma } from "../../utils/db";
+// import * as z from "zod";
+import { schema } from "../api/new-user";
+import useSWR from "swr";
+import axios from "axios";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Register: NextPage = () => {
+  const { data, error } = useSWR("/api/new-user", fetcher);
   // const router = useRouter();
 
   // model User {
@@ -18,22 +22,22 @@ const Register: NextPage = () => {
   //   workouts        Workout[]
   //   @@index([username])
   // }
-  const schema = z.object({
-    email: z
-      .string()
-      .email()
-      .trim()
-      .max(50, "Email cannot exceed 50 characters"),
-    name: z
-      .string()
-      .min(1, "Name must be at least 1 character")
-      .max(50, "Name cannot exceed 50 characters")
-      .optional(),
-    username: z
-      .string()
-      .min(5, "Username must be at least 5 characters")
-      .max(50, "Username cannot exceed 50 characters"),
-  });
+  // const schema = z.object({
+  //   email: z
+  //     .string()
+  //     .email()
+  //     .trim()
+  //     .max(50, "Email cannot exceed 50 characters"),
+  //   name: z
+  //     .string()
+  //     .min(1, "Name must be at least 1 character")
+  //     .max(50, "Name cannot exceed 50 characters")
+  //     .optional(),
+  //   username: z
+  //     .string()
+  //     .min(5, "Username must be at least 5 characters")
+  //     .max(50, "Username cannot exceed 50 characters"),
+  // });
 
   const {
     register,
@@ -43,18 +47,6 @@ const Register: NextPage = () => {
 
   const newAccount: SubmitHandler<FieldValues> = async (data) => {
     console.log("onSubmit:", data);
-    // const newUser = await prisma.user.create({
-    //   data: {
-    //     email: data.email,
-    //     name: data.name,
-    //     username: data.username,
-    //   },
-    // });
-    // const users = await prisma.user.findMany();
-    // console.log(users);
-    const res = await fetch("/api/new-user");
-    const newUser = await res.json();
-    console.log(newUser);
   };
 
   return (
