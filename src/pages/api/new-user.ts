637eef0d43a6ddac61f409252b7ce69cc7e79dda
Daniel Villegas <avill.daniel@gmail.com>
2022-07-25 12:32:30 -0700

    ci: able to register new users thru form validation from client & server

--- src/pages/api/new-user.ts
+++ src/pages/api/new-user.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import * as z from "zod";
 import { prisma } from "../../utils/db";
-import { userSchema, Schema } from "../../schemas/zodUserSchema";
+import { userSchema } from "../../schemas/zodUserSchema";
+import { Prisma } from "@prisma/client";
 
 // model User {
 //   id              Int               @id @default(autoincrement())
@@ -13,6 +14,7 @@ import { userSchema, Schema } from "../../schemas/zodUserSchema";
 //   workouts        Workout[]
 //   @@index([username])
 // }
+
 // export const schema = z.object({
 //   email: z.string().email().trim().max(50, "Email cannot exceed 50 characters"),
 //   name: z
@@ -34,30 +36,35 @@ export default async function handler(
   // test if data in request is valid and formatted
   try {
     const user = userSchema.parse(req.body);
-    res.send(user);
+    // console.log("user after initial Zod parse:", user);
 
     // if data passes validation, invoke db query with it
-    postUser(user);
-  } catch (error) {
-    // error handling is taken from Zod's docs
-    if (error instanceof z.ZodError) {
-      console.log(error.issues);
-      res.status(400).send(error.issues);
-    }
-  }
-  // res.send(data);
-  return;
-  // res.status(200).send(data);
-}
+    const newUser = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        username: user.username,
+      },
+    });
 
-const postUser = async (user: Schema) => {
-  console.log(user);
-  const newUser = await prisma.user.create({
-    data: {
-      email: user.email,
-      name: user.name,
-      username: user.username,
-    },
-  });
-  console.log("newUser:", newUser);
-};
+    return res.status(200).send(JSON.stringify(newUser));
+  } catch (e) {
+    // handle error when data does not conform to Zod schema
+    // error handling is taken from Zod's docs
+    if (e instanceof z.ZodError) {
+      console.log(e.issues);
+      return res.status(400).send(e.issues);
+    }
+
+    // handle error when invalid data is used in prisma schema query
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        console.log(
+          "There is a unique constraint violation. A new user cannot be created."
+        );
+        return res.status(401).send(e.meta);
+      }
+    }
+    // return res.status(400).send(e);
+  }
+}
--- src/pages/u/register.tsx
+++ src/pages/u/register.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { userSchema } from "../../schemas/zodUserSchema";
+import axios from "axios";
+import { useState } from "react";
 
 const Register: NextPage = () => {
   // const router = useRouter();
@@ -33,94 +35,54 @@ const Register: NextPage = () => {
   //     .max(50, "Username cannot exceed 50 characters"),
   // });
 
+  const [invalidMsg, setInvalidMsg] = useState("");
+
+  // instantiate useForm() which will conform to our defined Zod schema
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: zodResolver(userSchema) });
 
+  // if form data passes Zod schema, handle submit by invoking api/new-user with the valid data
   const newAccount: SubmitHandler<FieldValues> = async (data) => {
     console.log("onSubmit:", data);
+
+    // Query db with form data
+    //  if success, re-direct to user's new dashboard
+    //  if fails, render invalid error
+    try {
+      const res = await axios.post("/api/new-user", data);
+      setInvalidMsg("");
+      // USE ROUTER.PUSH HERE FOR REDIRECATION
+      console.log("res:", res);
+      console.log(
+        `Account ${res.data.username} has been registered. Re-direct to user dashboard.`
+      );
+    } catch (error: any) {
+      // console.error(error);
+      setInvalidMsg(error.response.data.target[0]);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(newAccount)}>
-      <input type="text" {...register("email")} placeholder="Email" />
-      {errors.email?.message && <p>{errors.email?.message}</p>}
+    <>
+      <form onSubmit={handleSubmit(newAccount)}>
+        <input type="text" {...register("email")} placeholder="Email" />
+        {errors.email?.message && <p>{errors.email?.message}</p>}
 
-      <input type="text" {...register("name")} placeholder="Name" />
-      {errors.name?.message && <p>{errors.name?.message}</p>}
+        <input type="text" {...register("name")} placeholder="Name" />
+        {errors.name?.message && <p>{errors.name?.message}</p>}
 
-      <input type="text" {...register("username")} placeholder="Username" />
-      {errors.username?.message && <p>{errors.username?.message}</p>}
+        <input type="text" {...register("username")} placeholder="Username" />
+        {errors.username?.message && <p>{errors.username?.message}</p>}
 
-      <input type="submit" value="Sign up" />
-    </form>
+        <input type="submit" value="Sign up" />
+      </form>
+
+      {invalidMsg && <p>Invalid {`${invalidMsg}`}</p>}
+    </>
   );
 };
 
 export default Register;
-
-// import { NextPage } from "next";
-// import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// // import * as z from "zod";
-// import { schema } from "../api/new-user";
-
-// const Register: NextPage = () => {
-//   // const router = useRouter();
-
-//   // model User {
-//   //   id              Int               @id @default(autoincrement())
-//   //   memberSince     DateTime          @default(now())
-//   //   username        String            @db.VarChar(50) @unique
-//   //   email           String            @db.VarChar(50) @unique
-//   //   name            String?           @db.VarChar(50)
-//   //   exerciseStats   Exercise_stats[]
-//   //   workouts        Workout[]
-//   //   @@index([username])
-//   // }
-//   // const schema = z.object({
-//   //   email: z
-//   //     .string()
-//   //     .email()
-//   //     .trim()
-//   //     .max(50, "Email cannot exceed 50 characters"),
-//   //   name: z
-//   //     .string()
-//   //     .min(1, "Name must be at least 1 character")
-//   //     .max(50, "Name cannot exceed 50 characters")
-//   //     .optional(),
-//   //   username: z
-//   //     .string()
-//   //     .min(5, "Username must be at least 5 characters")
-//   //     .max(50, "Username cannot exceed 50 characters"),
-//   // });
-
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm({ resolver: zodResolver(schema) });
-
-//   const newAccount: SubmitHandler<FieldValues> = async (data) => {
-//     console.log("onSubmit:", data);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(newAccount)}>
-//       <input type="text" {...register("email")} placeholder="Email" />
-//       {errors.email?.message && <p>{errors.email?.message}</p>}
-
-//       <input type="text" {...register("name")} placeholder="Name" />
-//       {errors.name?.message && <p>{errors.name?.message}</p>}
-
-//       <input type="text" {...register("username")} placeholder="Username" />
-//       {errors.username?.message && <p>{errors.username?.message}</p>}
-
-//       <input type="submit" value="Sign up" />
-//     </form>
-//   );
-// };
-
-// export default Register;
