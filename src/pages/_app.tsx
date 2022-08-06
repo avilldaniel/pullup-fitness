b9a3a30f9983d9250176452b08cc3ebad338463a
Daniel Villegas <avill.daniel@gmail.com>
2022-08-05 17:20:23 -0700

    chore: install RQ dependencies, branch out and convert state management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,31 @@
 import { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "../components/Layout";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
+  // instantiate react query client
+  const queryClient = new QueryClient();
+
   return (
     <Layout>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          // colorScheme: "dark",
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            // colorScheme: "dark",
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+// home page will re-direct user to login page
+// this is temporary while we figure out auth
+
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
