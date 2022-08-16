cfe4698939872407d8b94050519fc684bb33abeb
Daniel Villegas <avill.daniel@gmail.com>
2022-08-15 19:42:45 -0700

    refactor: testing mobile select dropdown

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -11,7 +11,7 @@ const Meta = ({ keywords, description, title }: IMetaProps) => {
     <Head>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no"
+        content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
       {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
