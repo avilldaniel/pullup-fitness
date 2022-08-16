2db0407cfae3123bc5614603c2ef05b8f62f240e
Daniel Villegas <avill.daniel@gmail.com>
2022-08-15 19:51:11 -0700

    refactor: testing mobile select dropdown

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -9,10 +9,15 @@ export interface IMetaProps {
 const Meta = ({ keywords, description, title }: IMetaProps) => {
   return (
     <Head>
+      {/* This tag ensures that <Select /> does not zoom in on mobile */}
       <meta
         name="viewport"
         content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
+
+      {/* For mobile compatibility? */}
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
       <meta name="keywords" content={keywords} />
