96f7d1ccb1748e5f22a8beeed5f2e0aee0ff11ec
Daniel Villegas <avill.daniel@gmail.com>
2022-09-18 15:48:14 -0700

    style: nav

--- src/components/main/Nav.tsx
+++ src/components/main/Nav.tsx
@@ -1,3 +1,4 @@
+import { Button, Divider } from "@mantine/core";
 import { IconJumpRope, IconYoga } from "@tabler/icons";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
@@ -18,19 +19,33 @@ const Nav: FC<NavProps> = ({}) => {
             </button>
           </Link>
 
+          <Divider orientation="vertical" mx="xs" my={8} color="dark" />
+
           <Link href={"/u/workout"}>
             <button>
               <IconYoga />
             </button>
           </Link>
+          <Divider orientation="vertical" mx="sm" my={8} color="dark" />
         </section>
 
         <section className={nav.links}>
           <Link href={"/u/stats"}>Stats</Link>
+          <Divider orientation="vertical" mx="sm" my="xs" color="dark" />
           <Link href={"/u/workout"}>Workouts</Link>
+          <Divider orientation="vertical" mx="sm" my="xs" color="dark" />
         </section>
 
-        <button onClick={() => signOut()}>Log Out</button>
+        {/* <button onClick={() => signOut()}>Log Out</button> */}
+        <Button
+          type="button"
+          // variant="filled"
+          size="xs"
+          style={{ backgroundColor: "#c81e4c", padding: "0 .7em" }}
+          onClick={() => signOut()}
+        >
+          Log Out
+        </Button>
       </main>
     </div>
   );
